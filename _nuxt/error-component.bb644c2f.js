073de41515ded609572c3df7b5e11a7e98567d59
kurt-liao <vermouth0226@gmail.com>
2022-11-03 01:12:14 +0800

    64ad0c3

---
+++ _nuxt/error-component.bb644c2f.js
@@ -0,0 +1,1 @@
+import{d as e,c as r,o,a}from"./entry.fd7e71a4.js";const _=e({__name:"error",setup(t){return r({redirect:"/"}),(c,n)=>(o(),a("div"))}});export{_ as default};
---
+++ search/_payload.js
@@ -0,0 +1,1 @@
+export default {data:{},prerenderedAt:1667409039357}
